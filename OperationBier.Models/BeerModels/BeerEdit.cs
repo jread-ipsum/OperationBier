499f77fc549cbb4322c20a9232792d39e686c7d4
Jordan Read <jread6477@gmail.com>
2021-09-14 21:21:41 -0400

    Beer models, services, and controllers BasicCrud added

---
+++ OperationBier.Models/BeerModels/BeerEdit.cs
@@ -0,0 +1,19 @@
+﻿using OperationBier.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationBier.Models.BeerModels
+{
+    public class BeerEdit
+    {
+        public int BeerId { get; set; }
+        public string BeerName { get; set; }
+        public double ABV { get; set; }
+        public bool IsRecommended { get; set; }
+        public string BreweryName { get; set; }
+        public string StyleName { get; set; }
+    }
+}
--- OperationBier/Controllers/BeerController.cs
+++ OperationBier/Controllers/BeerController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using OperationBier.Models.BeerModels;
+using OperationBier.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,64 @@ using System.Web.Http;
 
 namespace OperationBier.Controllers
 {
+    [Authorize]
     public class BeerController : ApiController
     {
+        private BeerService CreateBeerService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var beerService = new BeerService(userId);
+            return beerService;
+        }
+
+        public IHttpActionResult Post([FromBody] BeerCreate beer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBeerService();
+
+            if (!service.CreateBeer(beer))
+                return InternalServerError();
+
+            return Ok("The beer has been successfully Added!");
+        }
+
+        public IHttpActionResult Get()
+        {
+            BeerService beerService = CreateBeerService();
+            var beers = beerService.GetBeers();
+            return Ok(beers);
+        }
+
+        public IHttpActionResult Get([FromUri]int id)
+        {
+            BeerService beerService = CreateBeerService();
+            var beer = beerService.GetBeerById(id);
+            return Ok(beer);
+        }
+
+        public IHttpActionResult Put([FromBody] BeerEdit beer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBeerService();
+
+            if (!service.UpdateBeer(beer))
+                return InternalServerError();
+
+            return Ok("The beer has been successfully updated");
+        }
+
+        public IHttpActionResult Delete([FromUri]int id)
+        {
+            var service = CreateBeerService();
+
+            if (!service.DeleteBeer(id))
+                return InternalServerError();
+
+            return Ok("The beer has been successfully deleted.");
+        }
     }
 }
