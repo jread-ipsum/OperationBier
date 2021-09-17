d7a2a45f38c24521a6e56130a7908212158d037f
thatseanmoore <spmoore19@gmail.com>
2021-09-16 21:14:37 -0400

    Brewery basic crud & controllers

--- OperationBier.Services/BreweryService.cs
+++ OperationBier.Services/BreweryService.cs
@@ -3,10 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OperationBier.Data;
+using OperationBier.Models.BreweryModels;
+using OperationBier.Models.RetailModels;
+
 
 namespace OperationBier.Services
 {
     public class BreweryService
     {
+        private readonly Guid _userId;
+
+        public BreweryService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateBrewery(BreweryCreate model)
+        {
+            var entity =
+                new Brewery()
+                {
+                    AuthorId = _userId,
+                    BreweryName = model.BreweryName,
+                    BreweryDescription = model.BreweryDescription,
+                    IsStillInBusiness = model.IsStillInBusiness,
+                    Address = model.Address,
+                    State = model.State,
+                    ZipCode = model.ZipCode,
+                    PhoneNumber = model.PhoneNumber,
+                    Email = model.Email,
+                    CountryOfOrigin = model.CountryOfOrigin
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Breweries.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<BreweryListItem> GetBreweries()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Breweries
+                    .Select(e => new BreweryListItem
+                    {
+                        BreweryId = e.BreweryId,
+                        BreweryName = e.BreweryName
+                    });
+                return query.ToArray();
+            }
+        }
+
+        public BreweryDetails GetBreweryById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx
+                    .Breweries
+                    .Single(e => e.BreweryId == id);
+                return
+                    new BreweryDetails
+                    {
+                        BreweryName = entity.BreweryName,
+                        BreweryDescription = entity.BreweryDescription,
+                        IsStillInBusiness = entity.IsStillInBusiness,
+                        Address = entity.Address,
+                        State = entity.State,
+                        ZipCode = entity.ZipCode,
+                        PhoneNumber = entity.PhoneNumber,
+                        Email = entity.Email,
+                        CountryOfOrigin = entity.CountryOfOrigin
+                    };
+            }
+        }
+
+        public bool UpdateBrewery(BreweryEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Breweries
+                    .Single(e => e.BreweryId == model.BreweryId);
+
+                entity.BreweryName = model.BreweryName;
+                entity.BreweryDescription = model.BreweryDescription;
+                entity.IsStillInBusiness = model.IsStillInBusiness;
+                entity.Address = model.Address;
+                entity.State = model.State;
+                entity.ZipCode = model.ZipCode;
+                entity.PhoneNumber = model.PhoneNumber;
+                entity.Email = model.Email;
+                entity.CountryOfOrigin = model.CountryOfOrigin;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool Brewery(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Breweries
+                    .Single(e => e.BreweryId == id);
+
+                ctx.Breweries.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
--- OperationBier/Controllers/BreweryController.cs
+++ OperationBier/Controllers/BreweryController.cs
@@ -4,10 +4,70 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using OperationBier.Models.BreweryModels;
+
 
 namespace OperationBier.Controllers
 {
+    [Authorize]
     public class BreweryController : ApiController
     {
+        private BreweryService CreateBreweryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var breweryService = new BreweryService(userId);
+            return breweryService;
+        }
+
+        public IHttpActionResult Post([FromBody] BreweryCreate brewery)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBreweryService();
+
+            if (!service.CreateBrewery(brewery))
+                return InternalServerError();
+
+            return Ok("The brewery has been successfully created!");
+        }
+
+        public IHttpActionResult Get()
+        {
+           BreweryService breweryService = CreateBreweryService();
+            var brewery = breweryService.GetBrewery();
+            return Ok(brewery);
+        }
+
+        public IHttpActionResult Get([FromUri] int id)
+        {
+            BreweryService breweryService = CreateBreweryService();
+            var brewery = breweryService.GetBreweryById(id);
+            return Ok(brewery);
+        }
+
+        public IHttpActionResult Put([FromBody] BreweryEdit brewery)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBreweryService();
+
+            if (!service.UpdateBrewery(brewery))
+                return InternalServerError();
+
+            return Ok("The brewery has been successfully updated");
+        }
+
+        public IHttpActionResult Delete([FromUri] int id)
+        {
+            var service = CreateBreweryService();
+
+            if (!service.DeleteBrewery(id))
+                return InternalServerError();
+
+            return Ok("The brewery has been successfully deleted.");
+        }
     }
 }
