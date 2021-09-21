09a839db17f65528aab4d348c9ecb73a75d79dee
thatseanmoore <84104399+thatseanmoore@users.noreply.github.com>
2021-09-21 18:43:23 -0400

    update2

---
+++ OperationBier.Models/BreweryModels/BreweryListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationBier.Models.BreweryModels
+{
+   
+    public class BreweryListItem
+        {
+            public int BreweryId { get; set; }
+            public string BreweryName { get; set; }
+
+        }
+    }
+
+
