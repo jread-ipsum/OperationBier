3fd75544939a7fd274968ed94acdd72b833527ea
Jeffrey Hackett <Jeffth92@gmail.com>
2021-09-21 19:57:45 -0400

    Style roughly complete on all levels

--- OperationBier/Controllers/StyleController.cs
+++ OperationBier/Controllers/StyleController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using OperationBier.Models.StyleModels;
+using OperationBier.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,6 +13,54 @@ namespace OperationBier.Controllers
     [Authorize]
     public class StyleController : ApiController
     {
-        private StyleService CreateStyleService();
+        private StyleService CreateStyleService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var styleService = new StyleService(userId);
+            return styleService;
+        }
+
+        public IHttpActionResult Post([FromBody] StyleCreate style) //create
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateStyleService();
+
+            if (!service.CreateStyle(style))
+                return InternalServerError();
+
+            return Ok("This style has been created successfully.");
+        }
+
+        public IHttpActionResult Get() //read
+        {
+            StyleService styleService = CreateStyleService();
+            var styles = styleService.GetStyles();
+            return Ok(styles);
+        }
+
+        public IHttpActionResult Put([FromBody] StyleEdit style) //update
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateStyleService();
+
+            if (!service.UpdateStyle(style))
+                return InternalServerError();
+
+            return Ok("This style was updated successfully.");
+        }
+
+        public IHttpActionResult Delete([FromUri]int id) //delete
+        {
+            var service = CreateStyleService();
+
+            if (!service.DeleteStyle(id))
+                return InternalServerError();
+
+            return Ok("This style has been deleted successfully");
+        }
     }
 }
