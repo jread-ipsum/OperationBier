0fb7d731d549e8efcc08213aa8977c086e3dfe29
Colin Koestler <KoestlerColinR@gmail.com>
2021-09-23 18:10:18 -0400

    Koestler/Commit 7/Updates to Retai.cs, Op.Bier.Models, RetailServices, and RetailController. Inital 1st commit of RetaiIsOnPremise(StretchGoal

---
+++ OperationBier.Models/RetailModels/RetailIsOnPremise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationBier.Models.RetailModels
+{
+    public class RetailIsOnPremise
+    {
+        public int RetailId { get; set; }
+        public string RetailName { get; set; }
+        public bool? IsOnPremise { get; set; }
+    }
+}
